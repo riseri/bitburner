dc2d1a5711fbf1f01c4b9a141533c078a2bb2284
seriyu <41549935+riseri@users.noreply.github.com>
2026-09-18 23:01:32 -0700

    Mirror reserved ports in Go test harness

--- test/go-test-helpers.cjs
+++ test/go-test-helpers.cjs
@@ -4,7 +4,7 @@ const vm = require('node:vm');
 const root = path.join(__dirname, '..');
 
 function loadGo(file = 'go-bot.js', extra = {}) {
-    const sandbox = { console, Date, PORTS: { GO_STATUS: 12 }, ...extra };
+    const sandbox = { console, Date, PORTS: { WORKER_EVENTS: 20, FLEET_STATUS: 19, CONTRACT_STATUS: 18, JIT_STATUS: 17, PROGRESSION_STATUS: 16, JIT_CONTROL: 15, PROGRESSION_ACTION: 14, STOCK_STATUS: 13, GO_STATUS: 12 }, ...extra };
     vm.createContext(sandbox);
     let all = '';
     for (const item of ['lib/dashboard.js', 'lib/go-strategy.js', 'lib/go-session.js', 'go-bot.js']) {
